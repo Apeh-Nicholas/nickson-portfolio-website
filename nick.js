fe2f09b1547149d2bef8ba92013fd4e1e6368652
Nickson-N100 <apehnicholas33@gmail.com>
2023-05-29 19:00:22 +0100

    first commit

---
+++ nick.js
@@ -0,0 +1,24 @@
+const text = document.querySelector(".sec-text");
+	const textLoad = () => {
+		setTimeout(() => {
+			text.textContent = "Freelancer";
+		}, 0);
+		setTimeout(() => {
+			text.textContent = "web developer";
+		}, 4000);
+		setTimeout(() => {
+			text.textContent = "video editor";
+		}, 8000);
+		setTimeout(() => {
+			text.textContent = "NDT Inspector";
+		}, 1200);
+		setTimeout(() => {
+			text.textContent = " Physicist (I.N.P)";
+		}, 1600);
+	}
+	textLoad();
+	// setInterval(textLoad, 1200);
+	// 1s =1000 milliseconds
+
+
+  document.getElementById("footer").innerHTML = "<p>&copy; " + new Date().getFullYear() + " designed by NICKSON N100 MEDIA. All rights reserved.</p>";
